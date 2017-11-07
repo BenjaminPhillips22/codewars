5b978fb2001d22f09415036184b04a2ce0df6549
Ben <benjamin.phillips22@gmail.com>
2017-11-07 15:45:17 +1100

    fixed

--- Maximum subarray sum.py
+++ Maximum subarray sum.py
@@ -10,16 +10,18 @@ subsequence in an array or list of integers:
 
 def maxSequence(arr):
     return max([
-                sum(arr[start:end])
+                sum(arr[start:(end+1)])
                 for start in range(len(arr))
                 for end in range(len(arr))
-                if sum(arr[start:end]) >= 0
+                if sum(arr[start:(end+1)]) >= 0
             ], default=0)
 
 
 # %%
 
-# tests
-print(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6)
+# test
+print(maxSequence([77, -2, 1, -3, 4, -1, 2, 1, -5, 4]))
 print(maxSequence([]) == 0)
 print(maxSequence([-1, -2, -3]) == 0)
+
+print([i for i in range(10)][0:9])
