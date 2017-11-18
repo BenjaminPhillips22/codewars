f504adf3c07f1a251a9a8dbd793c7eed5f181fee
Ben <benjamin.phillips22@gmail.com>
2017-11-18 18:20:43 +1100

    started

---
+++ write number in expanded form.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/write-number-in-expanded-form
+
+# %%
+
+
+def expanded_form(num):
+    return str(num)
+
+
+# %%
+
+# tests
+
+print(expanded_form(12))
+print(expanded_form(70304))
