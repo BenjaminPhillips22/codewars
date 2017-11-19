98cf661a04dd9ab42221a5b28b85fc032958645b
Ben <benjamin.phillips22@gmail.com>
2017-11-19 21:59:33 +1100

    not finished yet

--- String incrementer.py
+++ String incrementer.py
@@ -1,10 +1,13 @@
 # https://www.codewars.com/kata/string-incrementer
 
 # %%
+import re
 
 
 def increment_string(strng):
-    return strng
+    a = re.findall(pattern=r'[0-9]+', string=strng)
+    b = [str(j) for j in [1 if i == [] else int(i) for i in a]]
+    return b
 
 
 # %%
