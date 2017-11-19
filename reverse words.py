3cbddac07ca8ab8e8251d4d2871ad41ae6de3e59
Ben <benjamin.phillips22@gmail.com>
2017-11-19 20:50:49 +1100

    finished

--- reverse words.py
+++ reverse words.py
@@ -2,9 +2,9 @@
 
 # %%
 
-def reverse_words(str):
-    return str[::-1]
 
+def reverse_words(str):
+    return ' '.join([s[::-1] for s in str.split(sep=' ')])
 
 
 # %%
@@ -12,6 +12,7 @@ def reverse_words(str):
 # tests
 
 print(reverse_words("This is an example!"))
-print(reverseWords("An example!") == "nA !elpmaxe")
-print(reverseWords "double  spaces" == "elbuod  secaps")
+print(reverse_words("double  spaces"))
+print(reverse_words("An example!") == "nA !elpmaxe")
+print(reverse_words("double  spaces") == "elbuod  secaps")
 print(reverse_words("This is an example!") == "sihT si na !elpmaxe")
