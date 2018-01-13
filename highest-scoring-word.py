e154909548c62c63e0601cfc687d64060a5c9b78
Ben <benjamin.phillips22@gmail.com>
2018-01-14 10:07:11 +1100

    finished

--- highest-scoring-word.py
+++ highest-scoring-word.py
@@ -2,16 +2,19 @@
 
 
 def high(x):
-    return x
-
-
-
-
-
-
-
+    words = x.split(sep=" ")
+    word_score = list()
+    for word in words:
+        word_score.append(sum([ord(letter)-96 for letter in word]))
+    return words[word_score.index(max(word_score))]
 
 
 # tests
 
 print(high('man i need a taxi up to ubud') == 'taxi')
+
+# other solutions
+
+
+def high(x):
+    return max(x.split(), key=lambda k: sum(ord(c) - 96 for c in k))
