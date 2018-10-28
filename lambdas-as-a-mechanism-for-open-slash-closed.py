ab3ab9bc1ce7fe157c68c035ed633fdf370a9ab7
Ben <benjamin.phillips22@gmail.com>
2018-10-28 19:23:38 +1100

    more fun

--- lambdas-as-a-mechanism-for-open-slash-closed.py
+++ lambdas-as-a-mechanism-for-open-slash-closed.py
@@ -7,3 +7,20 @@ whispered = lambda greeting: greeting.lower()+'.'
 greet = lambda style, msg: style(msg)
 
 print(greet(style=shouted, msg='goodness gracious'))
+
+
+def plus_one(num):
+    return num + 1
+
+
+def squared(num):
+    return num*num
+
+
+def do_thing(func, num):
+    return func(num)
+
+
+print(do_thing(plus_one, 2))
+
+print(do_thing(squared, 2))
